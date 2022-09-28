4897423d7d236b2aeb42828b4e23ae2e1084584d
Davi Ferreira <daviferreiraaew@gmail.com>
2022-09-27 21:09:22 -0300

    add list 4

---
+++ lista 4/pesquisaIdadePeso.py
@@ -0,0 +1,12 @@
+mais90 = 0
+idades = 0
+for c in range(4):
+    idade = int(input())
+    idades += idade
+    peso = float(input())
+    if peso > 90:
+        mais90 += 1
+
+media = idades / 4
+print(f"Qtd pessoas > 90 Kg: {mais90}")
+print(f"Idade média: {media:.2f}")
